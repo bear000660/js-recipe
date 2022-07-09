de3a06e22cc12118293510617440c49594c00e2b
bear000660 <mnstapazuru661@gmail.com>
2022-07-09 13:43:27 +0900

    [3の倍数]の発展を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -26,3 +26,67 @@ const genkiFunction = function (number) {
     }
   }
 }
+
+// 発展★[FizzBuzz]
+const FizzBuzzFunction = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      if (i % 5 === 0) {
+        console.log("FizzBuzz")
+      } else {
+        console.log("Fizz")
+      }
+    }
+    if (i % 3 !== 0) {
+      if (i % 5 === 0) {
+        console.log("Buzz")
+      } else {
+        console.log(i)
+      }
+    }
+    // switch (i) {
+    //   case i % 3 === 0:
+    //     console.log("Fizz")
+    //     break
+    //   case i % 5 === 0:
+    //     console.log("Buzz")
+    //     break
+    //   case i % 15 === 0:
+    //     console.log("FizzBuzz")
+    //     break
+    // }
+  }
+}
+
+// 発展★★[３が大好き]
+const like3Function = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      console.log(`${i}!!!!!!!`)
+    } else {
+      var j = String(i)
+      if (j.includes(3)) {
+        console.log(`${i}!!!!!!!`)
+      } else {
+        console.log(i)
+      }
+    }
+  }
+}
+
+// 発展★★★[元気な秒針]
+// プログラム (1)
+let n = 1
+const genkiFunction1 = function () {
+  if (n % 3 === 0) {
+    console.log(`${n}!!!!!!!`)
+  } else {
+    console.log(n)
+  }
+  n += 1
+}
+const interval = function () {
+  setInterval(genkiFunction1, 1000)
+}
+// プログラム (2)
+document.onload = interval()
