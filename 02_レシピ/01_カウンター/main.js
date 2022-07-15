c0969b3319f48c6375362f2b0712286bf7514160
bear000660 <mnstapazuru661@gmail.com>
2022-07-15 19:02:56 +0900

    [Webアプリケーションと状態]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,21 @@
-const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
+// const display = document.getElementById("display")
+// const plusButton = document.getElementById("plus-button")
+// const minusButton = document.getElementById("minus-button")
+// const doubledButton = document.getElementById("doubled-button")
 
-let count = 0
+// let count = 0
 
-plusButton.onclick = function () {
-  count += 1
-  display.textContent = count
-}
+// plusButton.onclick = function () {
+//   count += 1
+//   display.textContent = count
+// }
+
+// minusButton.onclick = function () {
+//   count -= 1
+//   display.textContent = count
+// }
+
+// doubledButton.onclick = function () {
+//   count *= 2
+//   display.textContent = count
+// }
